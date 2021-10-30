3d3f0205c16093856f1950d52e82569da9dedcd3
Ujjwaldeep <ujjwal2699singh@gmail.com>
2021-10-30 17:56:32 +0530

    Navbar Setup

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color kColorGreen = Color(0xff42db84);
+const Color kColorGreenLight = Color(0xffd5eddf);
+
+const Color kColorBlue = Color(0xff116479);
+
+const Color kColorDark = Color(0xff2E2E2E);
+const Color kColorLight = Color(0xfff1f1ef);
---
+++ lib/mockoin.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:mockoin/constants.dart';
+import 'package:line_icons/line_icons.dart';
+
+class Mockoin extends StatefulWidget {
+  const Mockoin({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Mockoin> createState() => _MockoinState();
+}
+
+class _MockoinState extends State<Mockoin> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home'),
+    Text('Prices'),
+    Text('My Investments'),
+    Text('Account'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kColorLight,
+      body: SafeArea(
+        child: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      ),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: kColorLight,
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              gap: 6,
+              iconSize: 26,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              duration: const Duration(milliseconds: 400),
+              textStyle: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: kColorGreen,
+              ),
+              rippleColor: kColorGreenLight,
+              hoverColor: kColorGreenLight,
+              color: kColorGreen,
+              activeColor: kColorGreen,
+              tabBackgroundColor: kColorGreenLight,
+              tabs: const [
+                GButton(icon: LineIcons.home, text: 'Home'),
+                GButton(icon: LineIcons.barChartAlt, text: 'Prices'),
+                GButton(icon: LineIcons.wallet, text: 'Investments'),
+                GButton(icon: LineIcons.userAlt, text: 'Account'),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
